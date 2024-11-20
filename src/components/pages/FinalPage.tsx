85360c8326fe0a830cac71af994c2fa361e3a6c7
roonder <apontejuliam@gmail.com>
2024-11-19 22:13:59 -0400

    [update]: names and text

--- src/components/pages/FinalPage.tsx
+++ src/components/pages/FinalPage.tsx
@@ -26,13 +26,14 @@ export const FinalPage = forwardRef(({
                 <Image src={"/assets/pink-white-flower.svg"} width={120} height={120} alt="Decorative Vines" className="horizontal-reflect" />
             </div>
 
-            <div className="w-full h-24 p-2 absolute -translate-y-[50%] top-[50%] bg-inv-white border-2 border-inv-brown text-center">
+            <div className="w-full h-28 p-2 absolute -translate-y-[50%] top-[50%] bg-inv-white border-2 border-inv-brown text-center">
                 <Image src={"/assets/golden-leaves.png"} width={300} height={40} alt="Decorative Vines"     className="absolute -top-12 -translate-x-1/2 left-1/2" />
                 <Image src={"/assets/golden-leaves.png"} width={300} height={40} alt="Decorative Vines"     className="absolute -bottom-12 -translate-x-1/2 left-1/2" />
 
-                <h1 className={`${great_vibes.className} text-5xl mt-3 flex items-center justify-center gap-4`}>
+                <h1 className={`${great_vibes.className} text-4xl mt-3 flex items-center justify-center gap-4`}>
                     <span>Te</span><span>Esperamos</span>
                 </h1>
+                <p className="text-xs text-wrap">y recuerden, los niños descansando plácidamente en casa...</p>
             </div>
 
             <Image src={"/assets/red-heart.svg"} width={60} height={60} alt="Decorative Vines" className="absolute bottom-10 -translate-x-1/2 left-1/2" />
